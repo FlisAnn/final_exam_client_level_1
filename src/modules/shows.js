8ad5bec6546fb0a29dd96cb86a43eb9298c6bb85
flis_ann <ann.flismark@gmail.com>
2021-02-03 13:24:52 +0100

    Importing redux Setting up redux folder structure

---
+++ src/modules/shows.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+const getShowsData = {
+  async index() {
+    let response = await axios.get("/serier")
+    disp
+  }
+}
---
+++ src/state/store/initalState.js
@@ -0,0 +1,5 @@
+const initialState = {
+  showsFeed: []
+}
+
+export default initialState
