ac05af55535b48e8e8bb8011bf710e7c8ed59eab
flis_ann <ann.flismark@gmail.com>
2021-02-03 13:16:15 +0100

    Creating first test

--- cypress/integration/visitorCanViewShows.feature.js
+++ cypress/integration/visitorCanViewShows.feature.js
@@ -3,14 +3,17 @@ describe('Visitor can see a list shows', () => {
     beforeEach(() => {
       cy.server()
       cy.route({
-        method: "GET",
-        url: "http://localhost:3000/shows",
-        response: "fx:",
+        method: 'GET',
+        url: 'https://content.viaplay.se/pc-se/serier/samtliga',
+        response: 'fx:viaplay-tvshows.json',
       })
-      cy.visit("/")
-    });
-    it("successfully view all listed shows")
-    
+      cy.visit('/')
+    })
+
+    it('successfully view all listed shows', () => {
+      cy.get("[data-cy='shows-index']").within(() => {
+        cy.get('.title').should('contain', "Grey's Anatomy")
+      })
+    })
   })
-  
 })
